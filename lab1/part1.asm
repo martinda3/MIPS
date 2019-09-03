73f11c39a8aefb4986e6e111327c912b18612cd7
dajion <martinda3@vcu.edu>
2019-09-03 17:06:28 -0400

    Little Progress

--- lab1/part1.asm
+++ lab1/part1.asm
@@ -9,26 +9,39 @@
 .globl main
 
 main: 
-	li $v0, 4
-	la $a0, MSG1
-	syscall
+	li $v0, 4	# print string
+	la $a0, MSG1	# load address of MSG1
+	syscall		# Simulate
       
-	li $v0, 4
-	la $a0, MSG2
-	syscall
+	li $v0, 5	# Read Int
+	syscall		# Sim
       
-	li $v0, 5
-	syscall
-      
-	la $t6, N
-	sw $v0, 0($t6)
+	la $t6, N	# load address of N
+	sw $v0, 0($t6)	# N = $v0
+	lw $a0, N
+	beq $a0, 1, result
+	li $a0, 1
+	li $v0, 1	# print int
+	syscall		# Sim 	
+loop:	
+	add $t0, $zero, 1
+	add $t1, $zero, 1	
+result:
+	li $v0, 4	# print string
+	la $a0, MSG2	# load address of MSG2
+	syscall		# Simulate
+	
+		
+	li $v0, 1	# print int
+	lw $a0, N
+	syscall		# Sim 	
 	
 	li $v0, 10
 	syscall
      
 .data
-MSG1: .asciiz "The sum from 1 - N \n"
-MSG2: .asciiz "Enter a value for N: "
+MSG1: .asciiz "Enter a value that will sum 1 to N: "
+MSG2: .asciiz "The sum of 1 to N is "
 
 # alignment
 	.align 2
