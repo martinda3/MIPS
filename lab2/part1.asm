e1ed31957dcb5592986c000bb5b0f6f053b0c100
dajion <martinda3@vcu.edu>
2019-09-23 17:30:00 -0400

    takeinga  break

--- lab2/part1.asm
+++ lab2/part1.asm
@@ -5,16 +5,16 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 	.data
 MSG1: 	.asciiz "Enter a character string: "
-PASS: 	.asciiz "The string is a palindrome.\n"
-Fail: 	.asciiz "The string is not a palindrome.\n"
-EROR: 	.asciiz "Error!\n"
+PASS: 	.asciiz "\nThe string is a palindrome.\n"
+FAIL: 	.asciiz "\nThe string is not a palindrome.\n"
+EROR: 	.asciiz "\nError!\n"
 NEWL: 	.asciiz " "
 NEWD: 	.asciiz "\n"
-CHCK:	.asciiz
+CHCK:	.asciiz " "
 
-	.align 2
+	.align 4
 
-
+LAST:	.byte '\n'
 SIZE1:	.word 100
 COUNT:	.word 4
 INDEX:	.word 0
@@ -23,7 +23,7 @@ TEMP:	.space 4
 	.text
 	.globl main
 
-main:					# 
+main:			
 	li   $v0, 4			# Call: Print String
 	la   $a0, MSG1		# load MSG1
 	syscall				# Output
@@ -33,194 +33,74 @@ main:					#
 	li   $v0, 8			# Call: Read String
 	syscall				# Input 
 	
-	li   $v0, 4			# Call: Print String
-	la   $a0, CHCK		# load MSG1
-	syscall				# Output
-	sw   $a0, CHCK
-	
 LEN:
-	la   $t0, CHCK		# load base
-	lw   $t1, INDEX
-	sll  $t4, $t1, 2	# mul index X 4
-	add  $t2, $t0, $t4 	# add base and index
-	#lw   $t3, 0($t2)	# load value at index
+	la   $t0, CHCK		# Load address of input
+	lb   $t1, INDEX		# Load Index
+	add  $t2, $t0, $t1 	# Address of Index
+## Remove	
+	li   $v0, 11		# Call: Print Char
+	lb   $a0, 0($t2)	# Char at Index
+	syscall		
 
+    li   $v0, 4			# Call: Print String
+	la   $a0, NEWL		# Print a " "
+	syscall			
 	
-	li   $v0, 11		# Call: Print String
-	lw   $a0, 0($t2)	# Print char
+	li   $v0, 1			# Call: Print Integer
+	la   $a0, 0($t1)	# Address in Index
 	syscall
 	
 	li   $v0, 4			# Call: Print String
-	la   $a0, NEWL		# load MSG1
-	syscall				# Output
-	
-	li   $v0, 1			# Call: Print String
-	la   $a0, 0($t1)		# load MSG1
+	la   $a0, NEWD		# Show new line
 	syscall
+## Remove
+    
+	lb  $a0, 0($t2)	# Char at Index
+	lb  $t4, LAST		# Load \n
+    beq $a0, $t4, VAL	# Check for end of string
 	
-	li   $v0, 4			# Call: Print String
-	la   $a0, NEWD		# load MSG1
-	syscall
+	addi $t1, $t1, 1	# Add to Index
+	sw   $t1, INDEX		# Update Index
 	
-	addi $t1, $t1, 1
-	sw   $t1, INDEX
-	lw   $t0, SIZE1
-	bgt  $t3, $t0, VAL		# Jump if string is over
 	j    LEN
-	# else Count + 1
 	 
 VAL:
-	lw   $a0, INDEX		# Counter
+	addi $t1, $t1, -1	# Add to Index
+    sw   $t1, COUNT
+	sw   $t1, TEMP
+	li   $t1, 0
+	sw   $t1, INDEX
+	lw   $a0, COUNT		# Counter
 	li   $v0, 1			# Call: Print Integer
 	syscall				# Output
+
+LOOK:
+	lw   $t1, COUNT
+	lw   $t2, INDEX
+	add  $t3, $t0, $t1
+	add  $t4, $t0, $t2
+	
+#	lb   $a0, 0($t4) 
+#	li   $v0, 11		# Call: Print Char
+#	syscall		
+#	lb   $a0, 0($t3)
+#	li   $v0, 11		# Call: Print Char
+#	syscall		
+
+	lb   $a1, 0($t3)
+	beq  $a0, $a1, STEP1
+	# add and dec
+	bne  $a0, $a1, NOPE
+STEP1:
+
+
+
+NOPE:
+	li   $v0, 4			# Call: Print String
+	la   $a0, FAIL		# load MSG1
+	syscall				# Output
+	
 	 
-#FOR1:
-#	lw   $t0, INDEX		# INDEX = position in array
-#	lw   $t1, COUT1		# COUNT = Number of Values on array
-#	addi $t2, $t0, -1	# Itterating Index
-#	sll  $t2, $t2, 2	# Mulitpying Index by 4 = address of Index
-#	sw   $t2, TEMP		# Storing address of index
-#	la   $t3, ARRY1		# Base address of array
-#	add  $t4, $t3, $t2	# Adding Base address and address of index
-#	lw   $a0, 0($t4)	# Loading value at address of index
-#	li   $v0, 1			# Call: Print Integer
-#	syscall				# Output
-#	li   $v0, 4			# Call: Print String
-#	la   $a0, SPACE		# Formatting
-#	syscall				# Output
-#	addi $t0, $t0, 1	# Advance to next position in array
-#	sw   $t0, INDEX		# Store new position
-#	bge  $t1, $t0, FOR1	# Check for all values in array
-#
-#	li   $t0, 0			# Reset position
-#	sw   $t0, INDEX		# Store new position
-#	
-#	li   $v0, 4			# Call: Print String
-#	la   $a0, MSG2		# load MSG2
-#	syscall				# Output
-#
-#REV1:
-#	lw   $t1, COUT1
-#	addi $t2, $t1, -1
-#	sll  $t2, $t2, 2
-#	sw   $t2, TEMP
-#	la   $t3, ARRY1
-#	add  $t4, $t3, $t2
-#	lw   $a0, 0($t4)
-#	li   $v0, 1
-#	syscall
-#	li   $v0, 4
-#	la   $a0, SPACE
-#	syscall
-#	addi $t1, $t1, -1
-#	sw   $t1, COUT1	
-#	bgt  $t1, $t0, REV1
-#	
-#	li   $v0, 4		# Call: Print String
-#	la   $a0, MSG3		# load MSG1
-#	syscall			# Output
-#    
-#	li   $t0, 1
-#	sw   $t0, INDEX
-# 
-#FOR2:
-#	lw   $t0, INDEX
-#	lw   $t1, COUT2
-#	addi $t2, $t0, -1
-#	sll  $t2, $t2, 2
-#	sw   $t2, TEMP
-#	la   $t3, ARRY2
-#	add  $t4, $t3, $t2
-#	lw   $a0, 0($t4)
-#	li   $v0, 1
-#	syscall
-#	li   $v0, 4
-#	la   $a0, SPACE
-#	syscall
-#	addi $t0, $t0, 1
-#	sw   $t0, INDEX
-#	bge  $t1, $t0, FOR2
-#
-#	li   $t0, 1
-#	sw   $t0, INDEX
-#
-#	li   $v0, 4		# Call: Print String
-#	la   $a0, MSG4		# load MSG1
-#	syscall			# Output
-#
-#
-#	li   $t0, 0
-#	sw   $t0, INDEX
-#
-#REV2:
-#	lw   $t1, COUT2
-#	addi $t2, $t1, -1
-#	sll  $t2, $t2, 2
-#	sw   $t2, TEMP
-#	la   $t3, ARRY2
-#	add  $t4, $t3, $t2
-#	lw   $a0, 0($t4)
-#	li   $v0, 1
-#	syscall
-#	li   $v0, 4
-#	la   $a0, SPACE
-#	syscall
-#	addi $t1, $t1, -1
-#	sw   $t1, COUT2	
-#	bne  $t1, $t0, REV2
-#
-#	li   $t0, 1
-#	sw   $t0, INDEX
-#
-#	li   $v0, 4		# Call: Print String
-#	la   $a0, MSG5		# load MSG1
-#	syscall			# Output
-#
-#FOR3:
-#	lw   $t0, INDEX
-#	lw   $t1, COUT3
-#	addi $t2, $t0, -1
-#	sll  $t2, $t2, 2
-#	sw   $t2, TEMP
-#	la   $t3, ARRY3
-#	add  $t4, $t3, $t2
-#	lw   $a0, 0($t4)
-#	li   $v0, 1
-#	syscall
-#	li   $v0, 4
-#	la   $a0, SPACE
-#	syscall
-#	addi $t0, $t0, 1
-#	sw   $t0, INDEX	
-#	bge  $t1, $t0, FOR3
-#
-#	li   $t0, 0
-#	sw   $t0, INDEX
-#	
-#	li   $v0, 4		# Call: Print String
-#	la   $a0, MSG6		# load MSG1
-#	syscall			# Output
-#
-#REV3:
-#	lw   $t1, COUT3
-#	addi $t2, $t1, -1
-#	sll  $t2, $t2, 2
-#	sw   $t2, TEMP
-#	la   $t3, ARRY3
-#	add  $t4, $t3, $t2
-#	lw   $a0, 0($t4)
-#	li   $v0, 1
-#	syscall
-#	li   $v0, 4
-#	la   $a0, SPACE
-#	syscall
-#	addi $t1, $t1, -1
-#	sw   $t1, COUT3	
-#	bgt  $t1, $t0, REV3
-#	
-#	li   $v0, 4		# Call: Print String
-#	la   $a0, MSG7		# load MSG1
-#	syscall			# Output
 END:	
 	li   $v0, 10		# Call: Exit
 	syscall			# Exit
